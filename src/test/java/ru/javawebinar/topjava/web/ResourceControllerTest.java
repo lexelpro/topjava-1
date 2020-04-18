d5bc0db4f6df81e449e06bf7cd62a8fcb97bd285
Alex Belke <alex.belke.de@gmail.com>
2020-04-18 11:25:55 +0200

    wip

---
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web;
+
+import org.junit.jupiter.api.Test;
+
+public class ResourceControllerTest extends AbstractControllerTest {
+	@Test
+	void testStyles() throws Exception {
+//		perform(get(""))
+	}
+}
