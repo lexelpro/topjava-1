2950aeddbc50e1f6916210f22ac2138256e0f149
Alex Belke <alex.belke.de@gmail.com>
2020-04-19 10:20:31 +0200

    test css

--- src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
+++ src/test/java/ru/javawebinar/topjava/web/ResourceControllerTest.java
@@ -2,9 +2,15 @@ package ru.javawebinar.topjava.web;
 
 import org.junit.jupiter.api.Test;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class ResourceControllerTest extends AbstractControllerTest {
 	@Test
 	void testStyles() throws Exception {
-//		perform(get(""))
+		perform(get("/resources/css/style.css"))
+				.andDo(print())
+				.andExpect(status().isOk());
 	}
 }
